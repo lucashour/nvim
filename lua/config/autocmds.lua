0ce2ae4bf5b9c9fc79322593e5c08c3b58aae8ff
Lucas Hourquebie <lucashour1993@gmail.com>
2025-09-24 14:09:41 -0300

    Apply linter to all the files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,7 +3,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   command = [[%s/\s\+$//e]],
 })
 
-
 vim.api.nvim_create_augroup("myfiletypes", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
